e77c095e0f3c0332ccdfe27d41840fdb4e08636b
Brian Currie <brian.currie@hotmail.com>
2022-06-15 10:32:57 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ monsterData }}
+  </div>
+</template>
+
+<script>
+const url = "https://www.dnd5eapi.co/api/monsters/adult-blue-dragon";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      monsterData: {},
+      monsterName: "a",
+    };
+  },
+  methods: {},
+  async mounted() {
+    this.monsterData = await this.$axios.$get(url);
+  },
+};
+</script>
